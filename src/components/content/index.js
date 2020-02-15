ab760bd897e05ab637f1321b44f35d931f89902f
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2020-02-15 22:34:30 +0000

    Add basic theme

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Style from "./content.module.scss";
 const Content = ({ children }) => {
     return (
-        <div className="col-12 col-md-12 col-xl-9 bg-dark p-5">
+        <div className={Style.themeLight + " col-12 col-md-12 col-xl-9 p-5"}>
             {children}
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1 className="text-light">Test</h1>
+    <h1>Test</h1>
   </Layout>
 )
 
