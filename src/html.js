db407f8d3c0839b7e4cb815e50cdad190892c38e
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2020-04-07 02:54:15 +0100

    Add loader

---
+++ src/html.js
@@ -0,0 +1,39 @@
+import React from "react"
+import PropTypes from "prop-types"
+import LoadingScreen from "./components/loader-screen";
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+
+        <div key={`loader`} id="___loader" >
+          <LoadingScreen />
+        </div>
+
+        <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
+
+        {props.postBodyComponents}
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
