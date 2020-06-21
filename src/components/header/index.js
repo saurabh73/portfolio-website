e6986eba6ede4697e5a8e14bd995c3221647f0f3
Saurabh Dutta <saurabhdutta.jk@gmail.com>
2020-06-21 03:32:41 +0100

    scss based device detection

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -17,26 +17,28 @@ const Header = ({ activePage, isMenuActive, setIsMenuActive }) => {
       <div className="container">
         <div className="row mx-0 w-100 justify-content-end">
           <div className="col-12 col-md-12 col-xl-9 px-0">
-            {isMobile &&
-              <div className="d-flex justify-content-between">
-                <Hamburger
-                  active={menuStateActive}
-                  type="squeeze"
-                  onClick={() => {
-                    console.log(`Menu Clicked ${menuStateActive}`);
-                    setMenuStateActive(!menuStateActive);
-                    setIsMenuActive(!menuStateActive);
-                  }}
-                />
-                <span className="mobile-page-name">{`/${activePage}`}</span>
-                <ThemeSwitcher></ThemeSwitcher>
-              </div>
-            }
-
-            <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
-              <PageMenu activePage={activePage}></PageMenu>
-              {!isMobile && <ThemeSwitcher></ThemeSwitcher>}
+            {/* Mobile Header */}
+            <div className="d-flex d-sm-none justify-content-between">
+              <Hamburger
+                active={menuStateActive}
+                type="squeeze"
+                onClick={() => {
+                  console.log(`Menu Clicked ${menuStateActive}`);
+                  setMenuStateActive(!menuStateActive);
+                  setIsMenuActive(!menuStateActive);
+                }}
+              />
+              <span className="mobile-page-name">{`/${activePage}`}</span>
+              <ThemeSwitcher></ThemeSwitcher>
             </div>
+
+
+            {/* Desktop Header */}
+            <div className="d-none d-sm-flex collapse navbar-collapse justify-content-between" id="navbarNav">
+              <PageMenu activePage={activePage}></PageMenu>
+              <ThemeSwitcher></ThemeSwitcher>
+            </div>
+
           </div>
         </div>
       </div>
